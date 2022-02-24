be81a2b55d0973461c3ec80ac6718497f80a553c
Joshua <ezejoshuac@gmail.com>
2022-02-24 03:39:45 +0100

    Create function for single round of the game

--- script/script.js
+++ script/script.js
@@ -1,7 +1,29 @@
 //function for computer random choices
 function computerPlay(){
-    let compChoice = ['Rock', 'Paper', 'Scissor'];
+    let compChoice = ['rock', 'paper', 'scissors'];
     return compChoice[Math.floor(Math.random() * compChoice.length)];
 }
 
-console.log(computerPlay());
+
+
+//Create function for single round of the game
+//Return a string that declares the winner
+function playRound(playerChoice, computerChoice){
+    if(playerChoice === 'rock' && computerChoice === 'scissors'){
+        return `You won! ${playerChoice} beats ${computerChoice}`;
+    }else if(playerChoice === 'scissors' && computerChoice === 'paper'){
+        return `You won! ${playerChoice} beats ${computerChoice}`;
+    }else if(playerChoice === 'paper' && computerChoice === 'rock'){
+        return `You won! ${playerChoice} beats ${computerChoice}`;
+    }else if( playerChoice === computerChoice){
+        return 'It\'s a tie';
+    }else{
+        return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+}
+
+const playerChoice = prompt('rock paper scissors', 'enter choice').toLowerCase();
+console.log(playerChoice);
+const computerChoice = computerPlay();
+console.log(computerChoice);
+console.log(playRound(playerChoice, computerChoice));
