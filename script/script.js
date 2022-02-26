794d025e3d94581f94918d4b1ddf15c5bf1d20fd
Joshua <ezejoshuac@gmail.com>
2022-02-26 23:13:17 +0100

    Create five rounds of the game and return scores and log winner strings

--- script/script.js
+++ script/script.js
@@ -1,50 +1,63 @@
 let choice = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
-//function for computer random choices
-function computerPlay(){
+//function for computer to return random string choices from an array
+function getComputerInput(){
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-//function for player inputs to be case-insensitive
-function playerPlay(){
+//function for player inputs to return case-insensitive string
+function getPlayerInput(){
     let choicePlay;
     return choicePlay = prompt('Enter rock, paper or scissors').toLowerCase();
 }
 
 
 //Create function for single round of the game
-//Return a string that declares the winner
-function playRound(playerChoice, computerChoice){
+//Return scores and log a string that declares the winner
+function roundWinner(playerChoice, computerChoice){
 
     if(playerChoice === computerChoice){
-        playerScore;
-        computerScore;
-       return 'It\'s a tie';
+    
+       console.log('It\'s a tie');
     }else if((playerChoice === 'rock' && computerChoice === 'scissors') || 
     (playerChoice === 'scissors' && computerChoice === 'paper') || 
     (playerChoice === 'paper' && computerChoice === 'rock')){
-        playerScore += 1;
-        return `You won! ${playerChoice} beats ${computerChoice}`;
+        console.log(`You won! ${playerChoice} beats ${computerChoice}`);
+        return playerScore += 1;
     }else{
-        computerScore += 1
-        return `You lose! ${computerChoice} beats ${playerChoice}`;
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+        return computerScore += 1;
     }
 }
 
-
-//FUnction to create 5 times play time
-function game(){
+//Create fuction for five consecutive rounds of game
+function createRound(){
     for(let i = 0; i < 5; i++){
-        const playerChoice = playerPlay(); 
-        const computerChoice = computerPlay();
-        playRound(playerChoice, computerChoice); 
+        let playerChoice = getPlayerInput();
+        let computerChoice = getComputerInput();
+        roundWinner(playerChoice, computerChoice);
+        //console.log(roundWinner(playerChoice, computerChoice));
         console.log(playerChoice);
+        console.log(playerScore);
         console.log(computerChoice);
-        console.log(playRound(playerChoice, computerChoice));
+        console.log(computerScore);
     }
-    console.log(playerScore);
-    console.log(computerScore);
 }
 
-game();
+createRound();
+//Function to create 5 play times
+// function game(){
+//     for(let i = 0; i < 5; i++){
+//         const playerChoice = getPlayerInput(); 
+//         const computerChoice = getComputerInput();
+//         roundWinner(playerChoice, computerChoice); 
+//         console.log(playerChoice);
+//         console.log(computerChoice);
+//         console.log(roundWinner(playerChoice, computerChoice));
+//     }
+//     console.log(playerScore);
+//     console.log(computerScore);
+// }
+
+// game();
