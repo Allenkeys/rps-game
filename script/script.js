7762834faa5cafe82ac8c824d0c15c3a881dc219
Joshua <ezejoshuac@gmail.com>
2022-02-26 13:34:15 +0100

    Create csae insensitive player input

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
 let choice = ['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
 //function for computer random choices
 function computerPlay(){
     return choice[Math.floor(Math.random() * choice.length)];
+}
+
+//function for player inputs to be case-insensitive
+function playerPlay(){
+    let choicePlay;
+    return choicePlay = prompt('Enter rock, paper or scissors').toLowerCase();
 }
 
 
@@ -9,34 +17,34 @@ function computerPlay(){
 //Return a string that declares the winner
 function playRound(playerChoice, computerChoice){
 
-    if(playerChoice === 'rock' && computerChoice === 'scissors'){
+    if(playerChoice === computerChoice){
+        playerScore;
+        computerScore;
+       return 'It\'s a tie';
+    }else if((playerChoice === 'rock' && computerChoice === 'scissors') || 
+    (playerChoice === 'scissors' && computerChoice === 'paper') || 
+    (playerChoice === 'paper' && computerChoice === 'rock')){
+        playerScore += 1;
         return `You won! ${playerChoice} beats ${computerChoice}`;
-    }else if(playerChoice === 'scissors' && computerChoice === 'paper'){
-        return `You won! ${playerChoice} beats ${computerChoice}`;
-    }else if(playerChoice === 'paper' && computerChoice === 'rock'){
-        return `You won! ${playerChoice} beats ${computerChoice}`;
-    }else if( playerChoice === computerChoice){
-        return 'It\'s a tie';
     }else{
+        computerScore += 1
         return `You lose! ${computerChoice} beats ${playerChoice}`;
     }
 }
 
 
-
-
-
-
+//FUnction to create 5 times play time
 function game(){
     for(let i = 0; i < 5; i++){
-        const playerChoice = prompt('rock paper scissors', 'enter choice').toLowerCase(); 
+        const playerChoice = playerPlay(); 
         const computerChoice = computerPlay();
         playRound(playerChoice, computerChoice); 
         console.log(playerChoice);
         console.log(computerChoice);
         console.log(playRound(playerChoice, computerChoice));
-
     }
+    console.log(playerScore);
+    console.log(computerScore);
 }
 
 game();
